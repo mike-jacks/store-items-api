2fb962a1794289edf83afadd4cd910729cd6dd61
Mike Jacks <mjacks4816@xmail.dixietech.edu>
2024-04-10 12:53:05 -0600

    update main to add functions for load and save of json file

--- main.py
+++ main.py
@@ -11,49 +11,58 @@ app = FastAPI()
 with open("store_items.json", "r") as file: 
     store_items_json = json.load(file)
 
-store_items = []
 
-for item in store_items_json:
-    store_items.append(Item(**item))
-
-def new_id() -> int:
-    latest_id = store_items[-1].id 
+def load_items() -> list[Item]:
+    store_items = []
+    with open("store_items.json", "r") as file: 
+        store_items_json = json.load(file)
+    for item in store_items_json:
+        store_items.append(Item(**item))
+    return store_items
+    
+    
+def save_items(data: list[Item]):
+    with open("store_items.json", "w") as file:
+        json.dump([store_item.model_dump() for store_item in data], file, indent=2)
+        
+def new_id_from(data: list[Item]) -> int:
+    latest_id = data[-1].id 
     while True:
-        if latest_id not in [item.id for item in store_items]:
+        if latest_id not in [item.id for item in data]:
             break
         latest_id += 1
     return latest_id
 
+data = load_items()
+
 @app.get("/store_items")
 async def get_items():
-    return {"detail": store_items}
+    return {"detail": data}
 
 @app.post("/store_items")
 async def add_item(item: RequestItem):
-    item = Item(id=new_id(), **item.model_dump())
-    store_items.append(item)
-    with open("store_items.json", "w") as file:
-        json.dump([store_item.json() for store_item in store_items], file, indent=4)
+    item = Item(id=new_id_from(data=data), **item.model_dump())
+    data.append(item)
+    save_items(data)
     return {"detail": f"Item with id: {item.id} has been inserted."}
 
 @app.put("/store_items/{item_id}")
 async def update_item(item_id: int, new_item: RequestItem):
-    for item in store_items:
+    for item in data:
         if item.id == item_id:
             for attr, value in new_item.model_dump().items():
                 setattr(item, attr, value)
-            with open("store_items.json", "w") as file:
-                json.dump([store_item.json() for store_item in store_items], file, indent=4)
+            save_items(data)
             return {"detail": f"Item with id: {item.id} has been updated."}
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Id: {item_id} not found.")
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Item with id: {item_id} not found.")
 
 
 @app.delete("/store_items/{item_id}")
 async def delete_item(item_id: int):
-    for i, item in enumerate(store_items):
+    for i, item in enumerate(data):
         if item.id == item_id:
-            store_items.pop(i)
-            with open("store_items.json", "w") as file:
-                json.dump([store_item.json() for store_item in store_items], file, indent=4)
+            data.pop(i)
+            save_items(data)
             return {"detail": f"Item with id: {item_id} has been deleted."}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Item with id: {item_id} not found.")
             
